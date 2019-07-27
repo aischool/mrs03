9b998a9e7734b097baf3483312c4df9ae62ea965
aischool <aischool815@gmail.com>
2019-07-28 08:35:26 +0900

    section07-n07

--- src/hoc/WithClass.js
+++ src/hoc/WithClass.js
@@ -1,28 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-// const withClass = (WrappedComponent, className) => {
-//     return (props) => (
-//         <div className={className}>
-//             <WrappedComponent {...props} />
-//         </div>
-//     )
-// }
-
-const withClass = (WrappedComponent, className) => {
-    const WithClass = class extends Component {
-        render () {
-            return (
-                <div className={className}>
-                    <WrappedComponent ref={this.props.forwardedRef} {...this.props} />
-                </div>
-            )
-        }
-    }
-
-    return React.forwardRef((props, ref) => {
-        return <WithClass {...props} forwardedRef={ref} />
-    });
-}
-
+const withClass = props => (
+  <div className={props.classes}>{props.children}</div>
+);
 
 export default withClass;
